a54ca6f0092329fc9d2f28751fb157f48a144e77
AdyashaTarasia <72077129+AdyashaTarasia@users.noreply.github.com>
2022-12-23 23:08:29 +0530

    Update LoginController.java
    cross origin updated

--- src/main/java/com/fse/shoppingapp/controller/LoginController.java
+++ src/main/java/com/fse/shoppingapp/controller/LoginController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1.0/shopping")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class LoginController {
 
     Logger logger = LogManager.getLogger(LoginController.class);
