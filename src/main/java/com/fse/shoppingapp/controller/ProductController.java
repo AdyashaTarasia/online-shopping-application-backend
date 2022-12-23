5db2cef67f83d4f8dceaa1e6b4d8f4fee87edcc0
AdyashaTarasia <72077129+AdyashaTarasia@users.noreply.github.com>
2022-12-23 23:09:26 +0530

    Update ProductController.java
    cross origin updated

--- src/main/java/com/fse/shoppingapp/controller/ProductController.java
+++ src/main/java/com/fse/shoppingapp/controller/ProductController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1.0/shopping")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class ProductController {
 
     @Autowired
